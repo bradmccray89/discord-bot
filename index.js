ef05078a3d7eb1c5cf65dc7eab2e7b59cddc992a
Brandon McCray <bradmccray89@gmail.com>
2020-12-20 23:58:04 -0500

    changed it back

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
             break;
         case '318992296181891072':
             userName = 'Brandon';
-            fileName = './audio_clips/my_favorite_popsicle.mp3';
+            fileName = './audio_clips/fart.mp3';
             break;
         case '661775904799850531':
             userName = 'Jacob';
