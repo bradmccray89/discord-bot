3ebdb75d3325483b5784ad8140e97b62db5c9c29
Brandon McCray <bradmccray89@gmail.com>
2021-08-08 22:15:56 -0400

    added axios and overwatch command

---
+++ commands/overwatch.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+const axios = require('axios');
+
+module.exports = {
+  name: 'overwatch',
+  description: 'gets current arcade playlist',
+  async execute(message) {
+    axios
+      .get('https://overwatcharcade.today/api/v1/overwatch/today')
+      .then((response) => {
+        const data = response.data.data;
+        sendMessage(message, data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    function sendMessage(message, data) {
+      let modeList = ``;
+      data.modes.forEach((mode) => {
+        modeList += `\n- ${mode.name}`;
+      });
+      message.channel.send(`Todays Arcade Games on Overwatch:${modeList}`);
+    }
+  },
+};
