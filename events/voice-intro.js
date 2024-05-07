76aee4d9f4b1407c517e530a3afd984d83c6b7a3
Brandon McCray <bradmccray89@gmail.com>
2024-05-06 20:40:58 -0400

    update liams intro

--- events/voice-intro.js
+++ events/voice-intro.js
@@ -24,7 +24,7 @@ module.exports = {
 				break;
 			case '707469106873368658':
 				userName = 'Liam';
-				fileName = './audio_clips/german_soldier_song.mp3';
+				fileName = './audio_clips/beaners.mp3';
 				break;
 			case '318992296181891072':
 				userName = 'Brandon';
